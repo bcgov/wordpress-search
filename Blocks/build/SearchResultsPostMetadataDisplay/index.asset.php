7ac58b15d797c8251236396f44bd7cf696bc659f
ASpiteri-BCGov <49036255+ASpiteri-BCGov@users.noreply.github.com>
2025-06-25 13:52:43 -0700

    fixed edit display

--- Blocks/build/SearchResultsPostMetadataDisplay/index.asset.php
+++ Blocks/build/SearchResultsPostMetadataDisplay/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '57f6a071bde8a1967bdd');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '04ebd4b3b0bbaf8ae2c1');
--- Blocks/src/SearchResultsPostMetadataDisplay/edit/index.js
+++ Blocks/src/SearchResultsPostMetadataDisplay/edit/index.js
@@ -66,41 +66,14 @@ export default function Edit({ attributes, setAttributes }) {
 
 			<div {...blockProps}>
 				<div className="wp-block-wordpress-search-search-results-post-metadata-display">
-					<div className="post-metadata preview">
-						<h4 className="metadata-title">
-							Post Metadata (Preview)
-						</h4>
-						<div className="metadata-list">
-							<div className="metadata-item">
-								<span className="metadata-key">
-									Custom Field 1:
-								</span>
-								<span className="metadata-value">
-									Sample Value
-								</span>
-							</div>
-							<div className="metadata-item">
-								<span className="metadata-key">
-									Custom Field 2:
-								</span>
-								<span className="metadata-value">
-									Another Value
-								</span>
-							</div>
-							<div className="metadata-item">
-								<span className="metadata-key">Category:</span>
-								<span className="metadata-value">
-									Example Category
-								</span>
-							</div>
-						</div>
-						<p className="editor-note">
-							<em>
-								This block will dynamically display all metadata
-								for each post in the query loop on the frontend.
-							</em>
-						</p>
+					<div className="post-metadata">
+						Date updated: April 2, 2024 &nbsp;&nbsp; Decision: Approved &nbsp;&nbsp; Area: Farm Appeals &nbsp;&nbsp; Regulated marketing appeal: Chicken
 					</div>
+					<p className="editor-note">
+						<em>
+							This block will dynamically display all metadata for each post in the query loop on the frontend.
+						</em>
+					</p>
 				</div>
 			</div>
 		</>
