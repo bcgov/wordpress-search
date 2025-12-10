11b1f9078336a955762053ee7494f7b9523e3e26
Michael Haswell <Michael.Haswell@gov.bc.ca>
2025-12-10 13:35:11 -0800

    Add composer install step

--- playwright.config.js
+++ playwright.config.js
@@ -4,6 +4,12 @@ import baseConfig from '@wordpress/scripts/config/playwright.config.js';
 const config = defineConfig({
     ...baseConfig,
     testDir: 'tests/e2e',
+    projects: [
+        {
+            name: 'chromium',
+            use: { ...devices['Desktop Chrome'] },
+        },
+    ],
 });
 
 export default config;
