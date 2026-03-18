bfc787a143c753a89ba13d5144166a16915457d3
Shawn Turple <shawn.turple@gov.bc.ca>
2026-03-18 14:22:54 -0700

    DSWP-601 reverting versions back to 1.0.0, for those blocks with no changes

--- Blocks/build/SearchActiveFilters/index.asset.php
+++ Blocks/build/SearchActiveFilters/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '0a4fb395a3be2bc14b1c');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '7f2ca62f533a9cb62651');
