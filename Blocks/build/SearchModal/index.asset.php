0fe15a67558afa69b75d14b8aedcca3788ae2747
ASpiteri-BCGov <49036255+ASpiteri-BCGov@users.noreply.github.com>
2026-03-20 08:30:09 -0700

    cleanup of unused code

--- Blocks/build/SearchModal/index.asset.php
+++ Blocks/build/SearchModal/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '72d717d0926a00d515f9');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'b1e2286b9ecb4936dc57');
