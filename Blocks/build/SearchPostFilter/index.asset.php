77424fba08d78f13318eedb3c67e6b5face7ae6c
ASpiteri-BCGov <49036255+ASpiteri-BCGov@users.noreply.github.com>
2026-05-15 13:13:31 -0700

    allow-for-non-selection-on-post-type-filter

--- Blocks/build/SearchPostFilter/index.asset.php
+++ Blocks/build/SearchPostFilter/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'c60ce716f18d2f116a07');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '0e5fc440464f465505d3');
