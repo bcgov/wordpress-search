a43b2e387aaf9b03a009421219acabd7bb04f25c
ASpiteri-BCGov <49036255+ASpiteri-BCGov@users.noreply.github.com>
2026-05-20 11:13:44 -0700

    a fix for showing posts when posts are not selected for a post type filter.

--- tests/test-taxonomy-filter-queries.php
+++ tests/test-taxonomy-filter-queries.php
@@ -407,6 +407,31 @@ class TaxonomyQueryFilteringTest extends WP_UnitTestCase {
     }
 
     /**
+     * Query blocks that do not inherit the main query must receive URL post_type filters.
+     */
+    public function test_query_loop_block_query_vars_post_type_from_url() {
+        $_GET['post_type'] = 'page';
+        $_GET['s']          = 'test';
+
+        global $wp_query;
+        $wp_query->is_search = true;
+
+        $query = array(
+            'perPage'  => 10,
+            'postType' => 'post',
+            'inherit'  => false,
+        );
+
+        $filtered = $this->taxonomy_filter->filter_query_loop_block_query_vars( $query, null, 1 );
+
+        $this->assertSame( 'page', $filtered['post_type'] );
+        $this->assertArrayNotHasKey( 'postType', $filtered );
+
+        unset( $_GET['post_type'], $_GET['s'] );
+        $wp_query->is_search = false;
+    }
+
+    /**
      * Test post type determination from taxonomy filters
      *
      * What this tests:
